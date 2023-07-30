3f728865d63e7bd4e3687d86de504592b3aed3ef
Todd Whitelow <todd@whitelow.tech>
2023-07-30 07:53:54 -0600

    add database migrations and seeds

--- API/Program.cs
+++ API/Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
   opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors(opt =>
+{
+  opt.AddPolicy("CorsPolicy", policy =>
+  {
+    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+  });
+});
 
 var app = builder.Build();
 
@@ -23,8 +30,24 @@ if (app.Environment.IsDevelopment())
   app.UseSwaggerUI();
 }
 
+app.UseCors("CorsPolicy");
+
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope(); // Create a scope for the lifetime of the request
+var services = scope.ServiceProvider; // Get access to the services we've registered
+try
+{
+  var context = services.GetRequiredService<DataContext>(); // Get access to the DataContext
+  await context.Database.MigrateAsync(); // Migrate the database
+  await Seed.SeedData(context); // Seed the database
+}
+catch (Exception ex)
+{
+  var logger = services.GetRequiredService<ILogger<Program>>();
+  logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
