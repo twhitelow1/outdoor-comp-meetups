cea88bf45041d605eaeb4e9d33f9276ec9ec41f2
Todd Whitelow <todd@whitelow.tech>
2023-08-04 16:13:46 -0600

    add edit handler

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,34 @@
+using Domain;
+using MediatR;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+            }
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                activity.Title = request.Activity.Title ?? activity.Title;
+
+                await _context.SaveChangesAsync();
+
+                return Unit.Value;
+            }
+        }
+    }
+}
